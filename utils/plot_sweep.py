861ffec9b4b3d7e75dd8dcce7a1f50c4689d9fb3
muke <muke101@protonmail.com>
2024-01-03 16:55:05 +0000

    updated plot_sweep to plot both abs and diff cpi

--- utils/plot_sweep.py
+++ utils/plot_sweep.py
@@ -7,15 +7,17 @@ import sys
 import math
 
 class CPIValues:
-    def __init__(self, base_cpi=None, pnd_cpi=None):
+    def __init__(self, base_cpi=None, pnd_cpi=None, diff=None):
         self.base_cpi = base_cpi
         self.pnd_cpi = pnd_cpi
+        self.diff = diff
 
+home = "/work/home_moved/l50031074"
 results_dir = sys.argv[1]
 benchmark_names = ["perlbench.0", "perlbench.1", "perlbench.2", "mcf.0", "lbm.0", "xalancbmk.0", "x264.0", "x264.1", "x264.2", "deepsjeng.0", "leela.0", "nab.0", "xz.0", "xz.1"]
-weights = {"perlbench.0": 0.5, "perlbench.1": 0.5, "xz.0": 0.5, "xz.1": 0.5, "x264.0": 1/3, "x264.1": 1/3, "x264.2": 1/3}
+weights = {"perlbench.0": 1/3, "perlbench.1": 1/3, "perlbench.2": 1/3, "xz.0": 0.5, "xz.1": 0.5, "x264.0": 1/3, "x264.1": 1/3, "x264.2": 1/3}
 SSIT_range = [32, 128, 352, 567, 800, 1024]
-LFST_range = [32, 128, 1024]
+LFST_range = [32, 128, 512, 1024]
 clear_period_ratio_range = [122, 244, 488, 976]
 lfst_map = {bench: {lfst:{ssit:CPIValues() for ssit in SSIT_range} for lfst in LFST_range} for bench in benchmark_names}
 clear_map = {bench: {clear:{ssit:CPIValues() for ssit in SSIT_range} for clear in clear_period_ratio_range} for bench in benchmark_names}
@@ -24,7 +26,7 @@ def parse_results(results_dir):
     for d in os.listdir(results_dir):
         if 'png' in d: continue
         os.chdir(results_dir+"/"+d)
-        Popen.wait(Popen("python3 ~/PND-Loads/utils/cpi_cmp.py", shell=True))
+        Popen.wait(Popen("python3 "+home+"/PND-Loads/utils/cpi_cmp.py", shell=True))
         ssit_size, lfst_size, clear_period = [int(i) for i in d.split('_')]
         results = open(results_dir+"/"+d+"/differences").readlines()
         current_benchmark = None
@@ -42,9 +44,12 @@ def parse_results(results_dir):
                 result.base_cpi = float(line.strip().split(":")[1])
             elif current_benchmark and line.strip().split(":")[0] == "PND CPI":
                 result.pnd_cpi = float(line.strip().split(":")[1])
+            elif current_benchmark and line.strip().split(":")[0] == "CPI":
+                result.diff = float(line.strip().split(":")[1])
 
 def plot_benchmark(benchmark, results_map, results_dir, label, title):
-    fig, ax = plt.subplots()
+    _, abs_ax = plt.subplots()
+    _, diff_ax = plt.subplots()
 
     lines = []
 
@@ -54,39 +59,44 @@ def plot_benchmark(benchmark, results_map, results_dir, label, title):
             if results_map[param][ssit].base_cpi is None or results_map[param][ssit].pnd_cpi is None: print("a ssit value was not populated!"); exit(1)
         base_cpis = [results_map[param][ssit].base_cpi for ssit in ssits]
         pnd_cpis = [results_map[param][ssit].pnd_cpi for ssit in ssits]
-        line, = ax.plot(ssits, base_cpis, label=f'{label}={param}', marker='+', alpha=0.7)
-        ax.plot(ssits, pnd_cpis, label=f'{label}={param}', marker='o', color=line.get_color(), linestyle='--')
+        diff_cpis = [results_map[param][ssit].diff for ssit in ssits]
+        line, = abs_ax.plot(ssits, base_cpis, label=f'{label}={param}', marker='+', alpha=0.7)
+        abs_ax.plot(ssits, pnd_cpis, label=f'{label}={param}', marker='o', color=line.get_color(), linestyle='--')
         lines.append(line)
+        diff_ax.plot(ssits, diff_cpis, label=f'{label}={param}')
     legends = [Line2D([0], [0], color=line.get_color(), lw=2, label=line.get_label()) for line in lines]
-    ax.set_xlabel('SSIT Size')
-    ax.set_ylabel('CPI')
-    ax.set_title(title)
-    ax.legend(handles=legends)
+    abs_ax.set_xlabel('SSIT Size')
+    abs_ax.set_ylabel('CPI')
+    abs_ax.set_title(title+ "Absolute CPI")
+    abs_ax.legend(handles=legends)
+    diff_ax.set_xlabel('SSIT Size')
+    diff_ax.set_ylabel('CPI % Diff')
+    diff_ax.set_title(title+" - % Diff CPI")
+    diff_ax.legend()
     plt.savefig(results_dir+"/"+benchmark+".png", dpi=1200)
 
-def plot_average(results_map, label, title):
-    fig, ax = plt.subplots()
-
-    average_map = {lfst:{ssit:CPIValues(1,1) for ssit in SSIT_range} for lfst in LFST_range}
+def plot_average(results_map, param_range, results_dir, label, title):
+    average_map = {param:{ssit:CPIValues(1,1,0) for ssit in SSIT_range} for param in param_range}
     for bench in results_map:
         for param in results_map[bench]:
             for ssit in SSIT_range:
                 average_map[param][ssit].base_cpi *= results_map[bench][param][ssit].base_cpi*weights.get(bench, 1)
                 average_map[param][ssit].pnd_cpi *= results_map[bench][param][ssit].pnd_cpi*weights.get(bench, 1)
+                average_map[param][ssit].pnd_cpi += results_map[bench][param][ssit].diff*weights.get(bench, 1)
     for param in average_map:
         for ssit in average_map[param]:
-            average_map[param][ssit].base_cpi = math.pow(average_map[param][ssit].base_cpi, 1 / len(benchmark_names))
-            average_map[param][ssit].pnd_cpi = math.pow(average_map[param][ssit].pnd_cpi, 1 / len(benchmark_names))
+            #len(benchmark_names) - 5 because we want to treat different runs of the same workload as one data point
+            average_map[param][ssit].base_cpi = math.pow(average_map[param][ssit].base_cpi, 1 / (len(benchmark_names)-5))
+            average_map[param][ssit].pnd_cpi = math.pow(average_map[param][ssit].pnd_cpi, 1 / (len(benchmark_names)-5))
+            average_map[param][ssit].diff = average_map[param][ssit].diff / (len(benchmark_names)-5)
 
-    plot_benchmark("average", average_map, results_dir+"/lfst_ssit_search", label, title)
+    plot_benchmark("average", average_map, results_dir, label, title)
 
 parse_results(results_dir+"/lfst_ssit_search")
-#parse_results(results_dir+"/clear_period_ssit_search")
+parse_results(results_dir+"/clear_period_ssit_search")
 
-#benchmark = "x264.2"
-#plot_benchmark(benchmark, lfst_map[benchmark], "LFST", benchmark+": SSIT vs CPI Difference Over LFST (Clear Period Ratio = 244)")
 for benchmark in benchmark_names:
-    plot_benchmark(benchmark, lfst_map[benchmark], results_dir+"/lfst_ssit_search", "LFST Size", benchmark+": SSIT vs CPI Difference Over LFST (Clear Period Ratio = 244)")
-    #plot_benchmark(benchmark, lfst_map[benchmark], results_dir+"/clear_period_ssit_search", "Clear Period Ratio", benchmark+": SSIT vs CPI Difference Over Clear Period Ratio (LFST = SSIT)")
-plot_average(lfst_map, "LFST Size", "Average CPI Difference Across Spec As LFST/SSIT Vary")
-# plot_average(clear_map, "Clear Period Ratio", "Average CPI Difference Across Spec As Clear Period Ratio/SSIT Vary")
+    plot_benchmark(benchmark, lfst_map[benchmark], results_dir+"/lfst_ssit_search", "LFST Size", benchmark+": SSIT vs CPI Over LFST (Clear Period Ratio = 244)")
+    plot_benchmark(benchmark, clear_map[benchmark], results_dir+"/clear_period_ssit_search", "Clear Period Ratio", benchmark+": SSIT vs CPI Over Clear Period Ratio (LFST = SSIT)")
+plot_average(lfst_map, LFST_range, results_dir+"/lfst_ssit_search", "LFST Size", "Average CPI Across Spec As LFST/SSIT Vary")
+plot_average(clear_map, clear_period_ratio_range, results_dir+"/clear_period_ssit_search", "Clear Period Ratio", "Average CPI Across Spec As Clear Period Ratio/SSIT Vary")
--- utils/sweep_sizes.py
+++ utils/sweep_sizes.py
@@ -1,11 +1,12 @@
 from subprocess import Popen
 import os
 
-gem5 = "~/PND-Loads-old/gem5/"
+home = "/work/home_moved/l50031074"
+gem5 = home+"/PND-Loads-old/gem5/"
 cpu_config = gem5+"src/cpu/o3/BaseO3CPU.py"
 
 SSIT_range = [32, 128, 352, 567, 800, 1024]
-LFST_range = [32, 128, 1024]
+LFST_range = [32, 128, 512, 1024]
 clear_period_ratio_range = [122, 244, 488, 976]
 standard_clear_period_ratio = 244
 
@@ -13,7 +14,7 @@ def update_config(sed_command):
     p = Popen(sed_command + cpu_config, shell=True)
     Popen.wait(p)
 
-results_base_dir = "~/spec_results/lfst_ssit_search/"
+results_base_dir = home+"/spec_results/lfst_ssit_search/"
 for lfst_size in LFST_range:
     for ssit_size in SSIT_range:
         lfst_sed = r"sed -i 's/\(LFSTSize = Param.Unsigned(\)\([0-9]\+\)\(, \".*\"\)/\1"+str(lfst_size)+r"\3/' "
@@ -25,16 +26,16 @@ for lfst_size in LFST_range:
         os.chdir(gem5)
         build = Popen("scons build/ARM/gem5.fast -j 100", shell=True)
         Popen.wait(build)
-        os.chdir("~/PND-Loads/utils")
+        os.chdir(home+"/PND-Loads/utils")
         run = Popen("python3 run_all_chkpts.py", shell=True)
         Popen.wait(run)
         results_dir = "_".join([str(i) for i in [ssit_size, lfst_size, standard_clear_period_ratio]])
         os.makedirs(results_base_dir+results_dir, exist_ok=True)
-        os.chdir("~/PND-Loads/utils")
+        os.chdir(home+"/PND-Loads/utils")
         aggregate = Popen("python3 aggregate_all_stats.py " + results_base_dir+results_dir, shell=True)
         Popen.wait(aggregate)
 
-results_base_dir = "~/spec_results/clear_period_ssit_search/"
+results_base_dir = home+"/spec_results/clear_period_ssit_search/"
 for clear_period_ratio in clear_period_ratio_range:
     for ssit_size in SSIT_range:
         lfst_size = ssit_size
@@ -47,11 +48,11 @@ for clear_period_ratio in clear_period_ratio_range:
         os.chdir(gem5)
         build = Popen("scons build/ARM/gem5.fast -j 100", shell=True)
         Popen.wait(build)
-        os.chdir("~/PND-Loads/utils")
+        os.chdir(home+"/PND-Loads/utils")
         run = Popen("python3 run_all_chkpts.py", shell=True)
         Popen.wait(run)
         results_dir = "_".join([str(i) for i in [ssit_size, lfst_size, clear_period_ratio]])
         os.makedirs(results_base_dir+results_dir, exist_ok=True)
-        os.chdir("~/PND-Loads/utils")
+        os.chdir(home+"/PND-Loads/utils")
         aggregate = Popen("python3 aggregate_all_stats.py " + results_base_dir+results_dir, shell=True)
         Popen.wait(aggregate)
