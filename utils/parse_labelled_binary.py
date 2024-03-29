187ee8e3a1ce3fd0a48b8d74b1bfb0c7e5266b9f
muke101 <muke101@protonmail.com>
2024-03-29 16:02:09 +0000

    updated parse labelled binary

--- utils/parse_labelled_binary.py
+++ utils/parse_labelled_binary.py
@@ -1,38 +1,30 @@
 import r2pipe
 import sys
 
-compiler_output_file = sys.argv[1]
-binary = sys.argv[2]
-compiler_output = open(compiler_output_file).readlines()
-load_indexes = {}
+label_binary = sys.argv[1]
+verify_binary = sys.argv[2]
 
-for line in compiler_output:
-    if line.startswith("FUNC"):
-        func_name = line.split(":")[1]
-        array = line.split(":")[2]
-        counts = array.split(",")
-        load_indexes[func_name] = [int(i) for i in counts]
+r2 = r2pipe.open(label_binary)
 
-r2 = r2pipe.open(binary)
+addrs = [l.split()[0] for l in r2.cmd("/x ffffffff").split('\n') if len(l) > 0]
 
-funcs = [i for i in r2.cmdj('isj') if i['type'] == 'FUNC' and i['name'] in load_indexes]
+r2 = r2pipe.open(verify_binary)
 
 pnd_addrs = []
-for func in funcs:
-    addr = func['vaddr']
-    num_insts = func['size'] / 4
-    r2.cmd('s '+str(addr))
-    insts = r2.cmdj('pdj '+str(num_insts))
-    counts = load_indexes[func['name']]
-    loads = 0
-    for i in insts:
-        opcode = i['opcode']
-        if opcode.startswith('ldr') or opcode.startswith('ldur') or opcode.startswith('ldp'):
-            loads += 1
-            if loads in counts:
-                pnd_addrs.append(i['offset'])
 
-address_file = open(binary+"_pnd_address", "w")
+for addr in addrs:
+    inst = r2.cmdj('pdj 1 @'+str(addr))[0]
+    try:
+        opcode = inst['opcode'].strip()
+    except KeyError:
+        continue
+    if not opcode.startswith('ldr') and not opcode.startswith('ldur') and not opcode.startswith('ldp'):
+        if opcode != "invalid":
+            print("Warning: found a non-load instruction: ", opcode)
+        continue
+    pnd_addrs.append(int(addr,16))
+
+address_file = open(verify_binary+"_pnd_address", "w")
 for addr in pnd_addrs:
     address_file.write(str(addr)+"\n")
 address_file.close()
