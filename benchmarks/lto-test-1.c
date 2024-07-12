ea0edc01c03c920c59b71f776c74e1e0924d9fd5
muke <muke101@protonmail.com>
2024-07-12 18:41:49 +0100

    added lto benchmarks

---
+++ benchmarks/lto-test-1.c
@@ -0,0 +1,16 @@
+int main(int argc, char **argv) {
+	int a[100]; 
+	int b[100];
+	for (int i=0; i < argc; i++) {
+		a[i] = i*i;
+		b[i] = i;
+	}
+	if (argv[1]) {
+		for (int i=0; i < argc; i++) {
+			test(a, b, i);	
+			a[i] *= b[i];	
+		}
+	}
+
+	return a[argc];
+}
