ddc11ead339a8d0f529ea7c0c3df98bced495de5
muke101 <muke@protonmail.com>
2024-08-13 16:58:31 +0100

    finihsed updating plot spec

--- utils/plot_spec.py
+++ utils/plot_spec.py
@@ -3,7 +3,7 @@ import matplotlib
 import numpy as np
 import argparse
 
-benchmark_names = ["perlbench.0", "perlbench.1", "perlbench.2", "mcf.0", "lbm.0", "xalancbmk.0", "x264.0", "x264.1", "x264.2", "deepsjeng.0", "leela.0", "xz.0", "xz.1", "omnetpp.0", "gcc.0", "gcc.1", "gcc.2"]
+benchmark_names = ["perlbench.0", "perlbench.1", "perlbench.2", "gcc.0", "gcc.1", "gcc.2", "mcf.0", "lbm.0", "omnetpp.0", "xalancbmk.0", "x264.0", "x264.1", "x264.2", "deepsjeng.0", "leela.0", "xz.0", "xz.1"]
 
 parser = argparse.ArgumentParser(prog='plot spec', description='plot graphs')
 
@@ -31,24 +31,26 @@ for model in cpu_models:
             results[current_benchmark][model] = float(value)
 
 fig, ax = plt.subplots()
-fig.set_size_inches(12,10,forward=True)
+fig.set_size_inches(12,12,forward=True)
 bar_width = 0.25
 for c, model in enumerate(cpu_models):
-    bars = ax.bar(np.arange(len(benchmark_names))+(bar_width*c)-(bar_width/3), [results[name][model] for name in benchmark_names], width=bar_width)
+    bars = ax.bar(np.arange(len(benchmark_names))+(bar_width*c)-(bar_width/len(cpu_models)), [results[name][model] for name in benchmark_names], width=bar_width)
     for bar in bars:
         value = bar.get_height()
         if abs(round(value,1)) > 0.1:
-            ax.text(bar.get_x() + bar.get_width() / 2+(bar_width/3), value, str(round(value,1))+"%", ha='center', va='bottom' if value >=0 else 'top')
+            if value > 0: value += 0.025
+            elif value < 0: value -= 0.025
+            ax.text(bar.get_x() + bar.get_width() / 2+(bar_width/len(cpu_models)) - 0.05, value, str(round(value,1))+"%", ha='center', va='bottom' if value >=0 else 'top', rotation=90)
 
 ax.legend(labels=cpu_models, fontsize=16)#, loc='center left')
 
 for i in range(len(benchmark_names)):
-    plt.axvline(x=i-bar_width, color='grey', linestyle=':', linewidth=1)
+    plt.axvline(x=i-(0.75*bar_width), color='grey', linestyle=':', linewidth=1)
 
 plt.tick_params(axis='x',labelsize=14, rotation=60)
 plt.tick_params(axis='y',labelsize=12)
 plt.xticks(np.arange(len(benchmark_names)) + bar_width / 2, benchmark_names)
 ax.set_ylabel('Percent Change', fontsize=18)
-ax.set_title('CPI Difference - '+addr_types[0], fontsize=18)
+ax.set_title('CPI Difference - '+addr_types[0].replace('_',' '), fontsize=18)
 plt.tight_layout()
 plt.savefig('/work/muke/graphs/cpi/'+addr_types[0]+".png", dpi=300)
