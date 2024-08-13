35b886b4312dcfa0ad857e861343454abe361119
muke101 <muke@protonmail.com>
2024-08-13 16:12:06 +0100

    added nab addr files and updated utils

--- utils/parse_all_labelled.py
+++ utils/parse_all_labelled.py
@@ -6,8 +6,8 @@ spec_dir = "/work/muke/spec2017/benchspec/CPU/"
 benches = ["600.perlbench_s", "605.mcf_s", "619.lbm_s",
            "625.x264_s", "631.deepsjeng_s",
            "641.leela_s", "657.xz_s", 
-           "620.omnetpp_s", "602.gcc_s", "623.xalancbmk_s"]
-           #"638.imagick_s", "644.nab_s"]
+           "620.omnetpp_s", "602.gcc_s", "623.xalancbmk_s",
+            "644.nab_s"] #"638.imagick_s"]
 addr_file_type = sys.argv[1]
 os.makedirs("/work/muke/PND-Loads/addr_files/"+addr_file_type, exist_ok=True)
 
--- utils/run_models.py
+++ utils/run_models.py
@@ -10,10 +10,15 @@ parser = argparse.ArgumentParser(prog='run_models', description='run over multip
 
 parser.add_argument('--addr-types', type=str, required=True)
 parser.add_argument('--cpu-models', type=str, required=True)
+parser.add_argument('--benches', type=str, required=False)
 args = parser.parse_args()
 
 addr_types = args.addr_types.split(',')
 cpu_models = args.cpu_models.split(',')
+benches = ""
+if args.benches != None:
+    benches = args.benches 
+
 if addr_types[0] == 'all': addr_types = [addr_type for addr_type in os.listdir(addr_file_dir)]
 elif addr_types[0] == 'all-with-base': addr_types = [addr_type for addr_type in os.listdir(addr_file_dir)] + ['base']
 if cpu_models[0] == 'all': cpu_models = [model.split('.')[0] for model in os.listdir(cpu_moddel_dir)]
@@ -27,7 +32,7 @@ if 'base' in addr_types:
         if type(cp) != subprocess.CompletedProcess: cp.wait()
         scons = subprocess.Popen("scons build/ARM/gem5.fast -j 28 --with-lto", shell=True)
         scons.wait()
-        run = subprocess.Popen("python3 /work/muke/PND-Loads/utils/run_all_chkpts.py base "+model+" with_base" , shell=True)
+        run = subprocess.Popen("python3 /work/muke/PND-Loads/utils/run_all_chkpts.py base "+model+" with_base "+benches , shell=True)
         run.wait()
 
 addr_types = [a for a in addr_types if a != 'base']
@@ -38,5 +43,5 @@ for model in cpu_models:
     scons = subprocess.Popen("scons build/ARM/gem5.fast -j 28 --with-lto", shell=True)
     scons.wait()
     for addr_type in addr_types:
-       run = subprocess.Popen("python3 /work/muke/PND-Loads/utils/run_all_chkpts.py "+addr_type+" "+model+" without_base", shell=True)
+       run = subprocess.Popen("python3 /work/muke/PND-Loads/utils/run_all_chkpts.py "+addr_type+" "+model+" without_base "+benches, shell=True)
        run.wait()
